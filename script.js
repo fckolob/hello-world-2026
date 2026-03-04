e7a135a245714a8452d9975447136e684979a7fe
fckolob <131497920+fckolob@users.noreply.github.com>
2026-03-04 20:12:08 -0300

    explaining comments added

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+// Getting the message element.
+
 const message = document.getElementById("message");
+
+// Text of the message.
 
 const text = "HELLO WORLD!";
 let index = 0;
+
+// Using recursion to add a letter after a delay.
 
 function showNextLetter() {
     if (index < text.length) {
@@ -9,11 +15,15 @@ function showNextLetter() {
         index++;
         
         setTimeout(showNextLetter, 200);
+
+    // After the message is complete a CSS animation is aplied.
+
     } else {
         
         message.style.animation = "animate 2s ease-in-out";
     }
 }
 
+// Calling the execution of showNextLetter.
 
 showNextLetter();
